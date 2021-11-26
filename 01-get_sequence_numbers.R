69c05d8cea2e0bc0bb9caa6db331bf695472b3b4
ksidorczuk <sidorczuk.katarzyna17@gmail.com>
2021-11-26 18:09:12 +0100

    updated sequence number summary

--- 01-get_sequence_numbers.R
+++ 01-get_sequence_numbers.R
@@ -3,6 +3,7 @@ library(dplyr)
 library(biogram)
 
 tar_load(list.files("_targets/objects"))
+source("functions/get_sequence_numbers.R")
 
 TP_summ <- summarise_TP_info(c("cTP_loc_tp.exp_seqs", "cTP_loc.exp_tp_seqs", "cTP.mTP_loc_exp.tp_seqs", 
                                "cTP.mTP_loc.exp_tp_seqs", "mTP_loc_tp.exp_seqs", "mTP_loc.exp_tp_seqs"),
@@ -12,5 +13,5 @@ SP_summ <- summarise_SP_info(c("SP_secreted_sp.exp_seqs", "SP_secreted.er_sp.exp
                                "SP_secreted.exp_sp_seqs", "SP_sp_seqs", "SP_sp.exp_seqs"),
                              annotations_all)
 
-TM_summ <- summarise_TP_info(c("TM_exp_seqs", "TM_seqs"),
+TM_summ <- summarise_TM_info(c("TM_exp_seqs", "TM_seqs"),
                              annotations_tm)
