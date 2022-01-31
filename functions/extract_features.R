ac61cfff333917c1e18f10b2ba87f77541ae70cf
ksidorczuk <sidorczuk.katarzyna17@gmail.com>
2022-01-31 14:35:39 +0100

    added clustering based on aa composition

--- functions/extract_features.R
+++ functions/extract_features.R
@@ -1,7 +1,7 @@
-calculate_binary_ngram_matrix <- function(sequences) {
+calculate_binary_ngram_matrix <- function(sequences, k_vector, kmer_gaps_list) {
   count_multimers(sequences,
-                  k_vector = c(1, 2, 2, rep(3, 4)),
-                  kmer_gaps_list = list(NULL, NULL, 1, c(0, 0), c(1, 0), c(0, 1), c(1, 1)),
+                  k_vector = k_vector, # = c(1, 2, 2, rep(3, 4)),
+                  kmer_gaps_list = kmer_gaps_list, # = list(NULL, NULL, 1, c(0, 0), c(1, 0), c(0, 1), c(1, 1)),
                   kmer_alphabet = toupper(colnames(biogram::aaprop)),
                   with_kmer_counts = FALSE) %>%
     as.matrix() %>% 
@@ -19,3 +19,16 @@ calculate_properties <- function(sequences, prop_list) {
     encode_seq(sequences, ith_prop) 
   }) %>% setNames(prop_list) %>% do.call(cbind, .)
 }
+
+calculate_aa_comp_peptides <- function(dataset_list) {
+  lapply(names(dataset_list), function(i) {
+    lapply(names(dataset_list[[i]]), function(ith_prot) {
+      data.frame(table(factor(dataset_list[[i]][[ith_prot]], levels = c("A", "C", "D", "E", "F", "G", "H",
+                                                                        "I", "K", "L", "M", "N", "P", "Q",
+                                                                        "R", "S", "T", "V", "W", "Y")))/length(dataset_list[[i]][[ith_prot]])) %>%
+        setNames(c("Amino acid", "Frequency")) %>% 
+        mutate(dataset = i,
+               prot = ith_prot)
+    }) %>% bind_rows()
+  }) %>% bind_rows()
+} 
